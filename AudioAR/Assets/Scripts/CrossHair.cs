0417bf33a7822fc66040631305f8d137f6548f3c
ariskid@outlook.com <arislingo@163.com>
2022-11-08 23:12:00 -0500

    Add input actions and control schemes

--- AudioAR/Assets/Scripts/CrossHair.cs
+++ AudioAR/Assets/Scripts/CrossHair.cs
@@ -1,18 +1,62 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CrossHair : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody rb;
+    public float speed = 5f;
+
+    private KeyControls kc;
+    private PlayerInput playerInput;
+
+    [SerializeField]
+    private string currentActionMap;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        playerInput = GetComponent<PlayerInput>();
+
+        kc = new KeyControls();
+        kc.UI.Enable();
+        kc.Player.Disable();
+        currentActionMap = playerInput.currentActionMap.name;
+        // Fire
+        // kc.Player.Fire.performed += ShootBullet;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
+        // Move {Keyboard}
+        kc.Player.Move.ReadValue<Vector2>();
+        Vector2 inputVector = kc.Player.Move.ReadValue<Vector2>();
+        rb.AddForce(new Vector3(inputVector.x, inputVector.y, 0) * speed, ForceMode.Force);
+    }
+
+
+    public void ShootBullet(InputAction.CallbackContext context)
+    {
+        if (context.performed)    
+        {
+            Debug.Log($"Shoot + {transform.position}");
+        }
         
+    }
+    public void MissionStart(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Debug.Log($"Mission Start + {context}");
+            // Switch action map
+            playerInput.SwitchCurrentActionMap("Player");
+            kc.Player.Enable();
+            kc.UI.Disable();
+
+            currentActionMap = playerInput.currentActionMap.name;
+        }
+
     }
 }
